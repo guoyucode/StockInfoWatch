e18725e2056ed502a872c3233e295a684f77fb0a
guoyu <guoyumail@qq.com>
2019-07-09 17:48:22 +0800

    版本信息限制显示高度

--- src/renderer/page/setting.vue
+++ src/renderer/page/setting.vue
@@ -2,7 +2,7 @@
 	<div>
 
 		<el-collapse class="version" >
-			<el-collapse-item  name="1" >
+			<el-collapse-item  name="1"><!--style="max-height: 260px; overflow-y: auto;"-->
 				<template slot="title">
 					版本信息: {{version}}
 				</template>
@@ -16,6 +16,13 @@
 
 				<br/>
 				<div style="color: blue">近期版本更新: </div>
+
+				<div>新增功能: 完成快捷键设置, 数据条数等各种开关设置功能</div>
+				<div>新增功能: 完成快捷键设置, 数据条数等各种开关设置功能</div>
+				<div>新增功能: 完成快捷键设置, 数据条数等各种开关设置功能</div>
+				<div>新增功能: 完成快捷键设置, 数据条数等各种开关设置功能</div>
+				<div>新增功能: 完成快捷键设置, 数据条数等各种开关设置功能</div>
+
 				<div>新增功能: 完成快捷键设置, 数据条数等各种开关设置功能</div>
 				<div>新增功能: 新一财经,选股宝,云财经数据展示,及定时器定时拉取新数据</div>
 				<div>新增功能: 财联社电报,深交所互动易问答 数据展示,及定时器定时拉取新数据</div>
