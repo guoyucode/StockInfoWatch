875529adfab0cbdc29c72d76a37beb5f38778e22
guoyu <guoyumail@qq.com>
2019-07-09 10:08:01 +0800

    vue-devtool开发工具

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -8,16 +8,25 @@
 /* eslint-disable */
 
 // Install `electron-debug` with `devtron`
+import {BrowserWindow} from "electron";
+
 require('electron-debug')({ showDevTools: true })
 
 // Install `vue-devtools`
 require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
+
+  //需要翻墙,使用不了
+  /*let installExtension = require('electron-devtools-installer')
   installExtension.default(installExtension.VUEJS_DEVTOOLS)
     .then(() => {})
     .catch(err => {
       console.log('Unable to install `vue-devtools`: \n', err)
-    })
+    })*/
+
+  //使用本地离线版的vue-tool开发工具
+  let filePath = ".electron-vue/vue-devtools-4.1.5";
+  console.log("devtool-path:", filePath)
+  BrowserWindow.addDevToolsExtension(filePath)
 })
 
 // Require `main` process to boot app
