fbf5c69b463a871fbfd87eaa764d1722173e54db
guoyu <guoyumail@qq.com>
2019-08-02 17:32:04 +0800

    版本号升级

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
 
-import {createPersistedState} from 'vuex-electron'
 
 import modules from './modules'
 
@@ -11,8 +10,6 @@ let strict = process.env.NODE_ENV !== 'production'
 Vue.use(Vuex)
 
 let plugins = [
-  createPersistedState(),
-  //createSharedMutations()
 ]
 
 //如果不是生产环境, 打印日志
